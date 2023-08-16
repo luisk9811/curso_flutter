4b1d43c1c03eda0bbdb06530117426a07eb4fb0e
Luis Carlos Rodriguez Diaz <luicarod@bancolombia.com.co>
2023-08-15 21:53:55 -0500

    create project hello_dart_app

---
+++ hello_dart_app/bin/hello_dart_app.dart
@@ -0,0 +1,5 @@
+import 'package:hello_dart_app/hello_dart_app.dart' as hello_dart_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hello_dart_app.calculate()}!');
+}
---
+++ hello_dart_app/test/hello_dart_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:hello_dart_app/hello_dart_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
