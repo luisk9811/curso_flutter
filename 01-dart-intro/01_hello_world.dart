d3d6da0aea389e7fb1c721b14ec47d511aec36ec
Luis Carlos Rodriguez Diaz <luicarod@bancolombia.com.co>
2023-08-16 21:28:06 -0500

    Nuevo curso de flutter en Udemy - hello dart

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,9 @@
+void main() {
+  // var muyName = 'LuisK';
+  String myName = 'LuisK';
+
+  // Interpolacion de strings
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
+}
